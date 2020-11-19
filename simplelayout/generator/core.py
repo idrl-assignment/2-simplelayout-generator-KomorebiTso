320f2bf83b2c2ff9da2c49b20dea180acf031cdc
KomorebiTso <Komorebi0216@163.com>
2020-11-19 18:13:00 +0800

    try

--- simplelayout/generator/core.py
+++ simplelayout/generator/core.py
@@ -8,15 +8,37 @@ import numpy as np
 
 def generate_matrix(
     board_grid: int, unit_grid: int, unit_n: int, positions: list
-) -> np.ndarray:
-    """生成指定布局矩阵
+):
+    BJB = np.zeros((board_grid, board_grid))
+    length = int(board_grid / unit_grid)
+    length1 = int((board_grid**2) / unit_grid)
 
-    Args:
-        board_grid (int): 布局板分辨率，代表矩形区域的边长像素数
-        unit_grid (int): 矩形组件分辨率
-        unit_n (int): 组件数
-        positions (list): 每个元素代表每个组件的位置
-    Returns:
-        np.ndarray: 布局矩阵
-    """
-    raise NotImplementedError  # TODO: 实现布局矩阵的生成
+    BJB = np.lib.stride_tricks.as_strided(
+        BJB, shape=(unit_grid, unit_grid, length, length),
+        strides=BJB.itemsize.np.array([length1, length, board_grid, 1])
+    )
+
+    list1 = []
+    list2 = []
+    for i in positions:
+        list1.append((i - 1) // length)
+        list2.append((i - 1) % length)
+    z = list(zip(list1, list2))
+
+    fill = np.ones((unit_grid, unit_grid), int)
+    for i in z:
+        BJB[i] = fill
+
+    a = BJB[0]
+    for i in range(length - 1):
+        b = BJB[i + 1]
+        img1 = np.concatenate((a, b), axis=1)
+        a = img1
+
+    a = img1[0]
+    for i in range(length - 1):
+        b = img1[i + 1]
+        image = np.concatenate((a, b), axis=1)
+        a = image
+
+    return image
