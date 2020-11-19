b7630107db9ffcea34b3f3146958340d433f2857
KomorebiTso <Komorebi0216@163.com>
2020-11-19 18:13:34 +0800

    try

--- simplelayout/cli/cli_generate.py
+++ simplelayout/cli/cli_generate.py
@@ -1,9 +1,30 @@
 import argparse
+import sys
 
 
 def get_options():
     parser = argparse.ArgumentParser()
-    # TODO: 按 1-simplelayout-CLI 要求添加相应参数
+    parser = argparse.ArgumentParser(description='list the parameter')
+    parser.add_argument('--board_grid', type=int, help='布局板分辨率')
+    parser.add_argument('--unit_grid', type=int, help='矩形组件分辨率')
+    parser.add_argument('--unit_n', type=int, help='组件数')
+    parser.add_argument('--positions', type=int, nargs='*', help='矩形组件分辨率')
+    parser.add_argument(
+        '-o', '--outdir', type=str, default='example_dir', help='输出目录')
+    parser.add_argument(
+        '--file_name', type=str, default='example', help='输出文件名')
+    args = parser.parse_args()
 
+    if args.board_grid % args.unit_grid != 0:
+        print('布局板分辨率须整除组件分辨率')
+        sys.exit(1)
+
+    if len(args.positions) != args.unit_n:
+        sys.exit(1)
+
+    Max = max(args.positions)
+    Min = min(args.positions)
+    if not (Min >= 1 and Max <= int(args.board_grid / args.unit_grid) ** 2):
+        sys.exit(1)
     options = parser.parse_args()
     return options
--- simplelayout/generator/utils.py
+++ simplelayout/generator/utils.py
@@ -5,18 +5,22 @@
 from pathlib import Path
 import matplotlib.pyplot as plt
 import scipy.io as sio
+import os
 
 
 def save_matrix(matrix, file_name):
     # TODO: 存储 matrix 到 file_name.mat, mdict 的 key 为 "matrix"
-    raise NotImplementedError
+    sio.savemat('{}.mat'.format(file_name), {'matrix': matrix})
 
 
 def save_fig(matrix, file_name):
     # TODO: 将 matrix 画图保存到 file_name.jpg
-    raise NotImplementedError
+    plt.imshow(matrix)
+    plt.savefig(file_name + '.jpg')
 
 
 def make_dir(outdir):
     # TODO: 当目录 outdir 不存在时创建目录
-    raise NotImplementedError
+    if not os.path.exists(outdir):
+        path = Path('dir')
+        path.mkdir()
